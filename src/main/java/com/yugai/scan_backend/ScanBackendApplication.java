568f49eb151174c3b1c8f38567da4adcade64352
yugai <784787081@qq.com>
2026-04-21 07:59:32 +0800

    init project

---
+++ src/main/java/com/yugai/scan_backend/ScanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yugai.scan_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScanBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yugai/scan_backend/ScanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yugai.scan_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
